0339eb815246a21ac3cc9f0c20f28365838daae8
khagrael <69402928+khagrael@users.noreply.github.com>
2022-03-02 19:56:08 +0200

    Add files via upload

---
+++ workf.py
@@ -0,0 +1,32 @@
+import streamlit as st
+from PIL import Image
+
+
+
+imagebus = Image.open('busca.png')
+imagebus = imagebus.resize((180, 180))
+imagepapa = Image.open('papa.jpg')
+imagepapa = imagepapa.resize((180, 180))
+imgerrani = Image.open('erani.jpg')
+imgerrani = imgerrani.resize((180, 180))
+imgfried = Image.open('fried.jpg')
+imgfried = imgfried.resize((180, 180))
+sasimage = Image.open('sas.jpg')
+sasimage = sasimage.resize((180, 180))
+imgkenin = Image.open('kenin.jpg')
+imgkenin = imgkenin.resize((180, 180))
+imgapo = Image.open('apo.jpg')
+imgapo = imgapo.resize((180, 180))
+
+workdf = f'# WTA Factory\n ## Owner'
+st.markdown(workdf)
+st.image(imagepapa,"Sergio 'papa giorgi' Giorgi")
+st.markdown('## CEO')
+st.image(imgkenin, 'Alex Kenin')
+st.markdown('## Communications Officer')
+st.image(imgapo, "Apostolos 'apostolos' Tsitsipas")
+st.markdown('\n## Workforce\n### Supervisor')
+st.image(sasimage, 'Sasnovic Aliaksandra')
+st.markdown('### Workers')
+sasimage2 = st.image([imgerrani, imgfried, imagebus], ['Errani Sara','Friedsam Anna-Lena', 'Bucsa Cristina'])
+
